644dc99cb3f05a510602f0f5ab0e991c9bdb3f3e
Elena Viushkova <viushkova.elena@gmail.com>
2023-06-09 18:17:31 +0300

    added fixes

--- src/test/java/com/semrush/tests/TestBase.java
+++ src/test/java/com/semrush/tests/TestBase.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -17,16 +16,26 @@ public class TestBase {
 
         SelenideLogger.addListener("allure", new AllureSelenide());
 
+        String browser = System.getProperty("browser", "chrome");
+        String browserVersion = System.getProperty("browserVersion", "100");
+        String browserSize = System.getProperty("browserSize", "1920x1080");
+        String remoteURL = System.getProperty("selenideRemote");
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        Configuration.baseUrl = System.getProperty("baseUrl", "https://careers.semrush.com");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "113");
-//        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-        Configuration.timeout = 10000;
-        Configuration.holdBrowserOpen = true;
+        if (remoteURL != null) {
+            Configuration.remote = remoteURL;
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+        }
+
         Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "https://careers.semrush.com";
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
+        Configuration.browserSize = browserSize;
+
+
     }
 
     @AfterEach
@@ -35,7 +44,7 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
 
-        if (System.getProperty("selenide_remote") != null) {
+        if (System.getProperty("selenideRemote") != null) {
             Attach.addVideo();
         }
     }
