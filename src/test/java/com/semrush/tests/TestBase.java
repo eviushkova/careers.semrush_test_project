79782f16cff016f4cae21e5ebe6205bc42c4c010
Elena Viushkova <viushkova.elena@gmail.com>
2023-07-27 20:18:16 +0300

    added fixes

--- src/test/java/com/semrush/tests/TestBase.java
+++ src/test/java/com/semrush/tests/TestBase.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -19,53 +20,35 @@ public class TestBase {
     static SelenoidConfig selenoidConfig = ConfigFactory.create(SelenoidConfig.class, System.getProperties());
 
     @BeforeAll
-    static void beforeAll() {
-        Configuration.remote = System.getProperty("selenoid_url", selenoidConfig.selenoidUrl());
+    static void configure() {
+
+        String selenoidUrl = System.getProperty("selenoid_url");
+        String selenoidLoginPassword = System.getProperty("selenoid_login_password");
+        selenoidUrl = selenoidUrl.replaceAll("https://", "");
+        Configuration.remote = "https://" + selenoidLoginPassword + "@" + selenoidUrl;
+        Configuration.pageLoadStrategy = "eager";
+
+        Configuration.remote = System.getProperty("remoteBrowser", selenoidConfig.selenoidUrl());
+
         Configuration.baseUrl = System.getProperty("base_url", "https://careers.semrush.com");
-        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+
         String[] browser = System.getProperty("browser", "chrome:100.0").split(":");
         Configuration.browser = browser[0];
         Configuration.browserVersion = browser[1];
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 20000;
+
+        Configuration.browserVersion = System.getProperty("browser_version", "100");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+        Configuration.timeout = 10000;
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
+        Map<String, Object> prop = new HashMap<>();
+        prop.put("enableVNC", true);
+        prop.put("enableVideo", true);
+
+        capabilities.setCapability("selenoid:options", prop);
+
         Configuration.browserCapabilities = capabilities;
     }
-
-//    @BeforeAll
-//    static void configure() {
-//
-////        String selenoidUrl = System.getProperty("selenoid_url");
-////        String selenoidLoginPassword = System.getProperty("selenoid_login_password");
-////        selenoidUrl = selenoidUrl.replaceAll("https://", "");
-////        Configuration.remote = "https://" + selenoidLoginPassword + "@" + selenoidUrl;
-////        Configuration.pageLoadStrategy = "eager";
-//
-//        Configuration.remote = System.getProperty("remoteBrowser", selenoidConfig.selenoidUrl());
-//
-//        Configuration.baseUrl = System.getProperty("base_url", "https://careers.semrush.com");
-//
-//        String[] browser = System.getProperty("browser", "chrome:100.0").split(":");
-//        Configuration.browser = browser[0];
-//        Configuration.browserVersion = browser[1];
-//
-//        Configuration.browserVersion = System.getProperty("browser_version", "100");
-//        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
-//        Configuration.timeout = 10000;
-//
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        Map<String, Object> prop = new HashMap<>();
-//        prop.put("enableVNC", true);
-//        prop.put("enableVideo", true);
-//
-//        capabilities.setCapability("selenoid:options", prop);
-//
-//        Configuration.browserCapabilities = capabilities;
-//    }
 
     @BeforeEach
     void addListener() {
